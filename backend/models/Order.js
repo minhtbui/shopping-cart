f82d2506cdcdc6844f05cf8f27cd8616f70aed24
agl-buiminhtu <troy.bmt@gmail.com>
2021-03-24 18:54:44 +0700

    commit

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -33,6 +33,7 @@ const Order = mongoose.Schema(
             update_time: { type: String },
             email_address: { type: String },
         },
+        itemsPrice: { type: Number, required: true, default: 0.0 },
         taxPrice: { type: Number, required: true, default: 0.0 },
         shippingPrice: { type: Number, required: true, default: 0.0 },
         totalPrice: { type: Number, required: true, default: 0.0 },
